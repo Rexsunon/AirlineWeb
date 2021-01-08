4137938935ec5ad418ca4ed5be5f68a8c010e75e
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2021-01-08 03:18:40 +0100

    Made flight details migrations

--- Migrations/20210108021314_InitialAirlineAPIMigration.Designer.cs
+++ Migrations/20210108021314_InitialAirlineAPIMigration.Designer.cs
@@ -2,14 +2,16 @@
 using AirlineWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AirlineWeb.Migrations
 {
     [DbContext(typeof(AirlineDbContext))]
-    partial class AirlineDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210108021314_InitialAirlineAPIMigration")]
+    partial class InitialAirlineAPIMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
